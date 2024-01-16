4fc35a0999a564b0db69d43ea20d1a415dc6cabd
Irfan Muqorib <irfanmuqorib33@gmail.com>
2024-01-16 23:29:51 +0700

    fix api all

--- src/pages/api/admin/[id]/route.ts
+++ src/pages/api/admin/[id]/route.ts
@@ -1,17 +1,8 @@
-import { getSession } from "next-auth/react";
 import type { NextApiRequest, NextApiResponse } from "next";
 import prisma from "../../../../../server/db/client";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const session = await getSession({ req });
-
-  if (!session) {
-    res.status(401).json({ message: "Unauthorized" });
-    return;
-  }
-
   try {
-    await prisma.$connect();
     const { id } = req.query;
     const courses = await prisma.course.findUnique({
       where: { id: id as string },
@@ -19,11 +10,11 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
         joinedCourses: true,
       },
     });
-    res.status(200).json({ success: true, courses });
+    return res.status(200).json({ success: true, courses });
   } catch (error) {
     console.error("Error fetching courses:", error);
-    res.status(500).json({ success: false, message: "Internal Server Error" });
-  } finally {
-    await prisma.$disconnect();
+    return res
+      .status(500)
+      .json({ success: false, message: "Internal Server Error" });
   }
 };
--- src/pages/api/admin/[id]/updateCourse.ts
+++ src/pages/api/admin/[id]/updateCourse.ts
@@ -1,41 +1,24 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { getServerSession } from "next-auth";
-import { authOptions } from "../../auth/[...nextauth]";
 import prisma from "../../../../../server/db/client";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const session = await getServerSession(req, res, authOptions);
-  if (!session) {
-    res.status(401).json({ error: "Unauthorized" });
-    return;
-  } else {
-    try {
-      await prisma.$connect();
-      const { courseId } = req.query;
-      const courseData = req.body;
-      // If courseId is provided, update the existing course
-      const updatedCourse = await prisma.course.update({
-        where: { id: courseId as string },
-        data: courseData,
-      });
-
-      console.log(updatedCourse, "updatedCourse");
-      if (updatedCourse) {
-        res.status(200).json({
-          success: true,
-          message: "Course updated successfully",
-          course: updatedCourse,
-        });
-      } else {
-        res.status(404).json({ success: false, message: "Course not found" });
-      }
-    } catch (error) {
-      console.error("Error updating course:", error);
-      res
-        .status(500)
-        .json({ success: false, message: "Internal Server Error" });
-    } finally {
-      await prisma.$disconnect(); // Disconnect from the database
-    }
+  try {
+    const { courseId } = req.query;
+    const courseData = req.body;
+    // If courseId is provided, update the existing course
+    const updatedCourse = await prisma.course.update({
+      where: { id: courseId as string },
+      data: courseData,
+    });
+    return res.status(200).json({
+      success: true,
+      message: "Course updated successfully",
+      course: updatedCourse,
+    });
+  } catch (error) {
+    console.error("Error updating course:", error);
+    return res
+      .status(500)
+      .json({ success: false, message: "Internal Server Error" });
   }
 };
